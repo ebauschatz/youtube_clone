650f416c4361fe5eaefb0c209da009cb08d7be76
ebauschatz <eaenglish113@gmail.com>
2022-11-01 16:21:19 -0500

    feat: adding auth reply creation endpoint

--- backend/replies/urls.py
+++ backend/replies/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('comment/<int:comment_id>/', views.get_replies_by_comment_id),
+    path('comment/<int:comment_id>/', views.replies_by_comment_id),
 ]
--- backend/replies/views.py
+++ backend/replies/views.py
@@ -1,15 +1,21 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
-from django.shortcuts import get_object_or_404
 from rest_framework import status
 from .models import Reply
 from .serializers import ReplySerializer
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 @permission_classes([IsAuthenticated])
-def get_replies_by_comment_id(request, comment_id):
-    replies = Reply.objects.filter(comment_id=comment_id)
-    serializer = ReplySerializer(replies, many=True)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+def replies_by_comment_id(request, comment_id):
+    if request.method == 'GET':
+        replies = Reply.objects.filter(comment_id=comment_id)
+        serializer = ReplySerializer(replies, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = ReplySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user, comment_id=comment_id)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
