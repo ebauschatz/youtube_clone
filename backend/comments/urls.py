c77e68344bac7a90ef39e1a3b1355f4e46577c79
ebauschatz <eaenglish113@gmail.com>
2022-11-01 15:19:47 -0500

    feat: adding auth endpoint for creating new comments

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.user_comments),
     path('video/<video_id>/', views.get_comments_by_video_id),
 ]
--- backend/comments/views.py
+++ backend/comments/views.py
@@ -13,3 +13,14 @@ def get_comments_by_video_id(requsest, video_id):
     comments = Comment.objects.filter(video_id=video_id)
     serializer = CommentSerializer(comments, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def user_comments(request):
+    if request.method == 'POST':
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
